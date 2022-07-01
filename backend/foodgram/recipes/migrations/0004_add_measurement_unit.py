714d4697fcea68364f343eb35545ddf012f28c10
AlexStr94 <alex.strunskiy@ya.ru>
2022-07-01 20:33:13 +0300

    v.1.01 Beta add data migrations

---
+++ backend/foodgram/recipes/migrations/0004_add_measurement_unit.py
@@ -0,0 +1,47 @@
+import csv
+from pathlib import Path
+
+from django.db import migrations
+
+data_path = Path(__file__).resolve().parents[4] / 'data/ingredients.csv'
+try:
+    with open(data_path, encoding='utf-8') as f:
+        data = [list(line) for line in csv.reader(f)]
+except Exception:
+    data = []
+
+
+def add_measurement_unit(apps, schema_editor):
+    MeasurementUnit = apps.get_model('recipes', 'MeasurementUnit')
+    for ingredient in data:
+        try:
+            MeasurementUnit.objects.get_or_create(
+                name=ingredient[1]
+            )
+        except Exception:
+            continue
+
+
+def remove_measurement_unit(apps, schema_editor):
+    MeasurementUnit = apps.get_model('recipes', 'MeasurementUnit')
+    for ingredient in data:
+        try:
+            MeasurementUnit.objects.get(
+                name=ingredient[1]
+            ).delete()
+        except Exception:
+            continue
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recipes', '0003_alter_recipe_options'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            add_measurement_unit,
+            remove_measurement_unit
+        )
+    ]
---
+++ backend/foodgram/recipes/migrations/0005_add_ingredients.py
@@ -0,0 +1,54 @@
+import csv
+from pathlib import Path
+
+from django.db import migrations, transaction
+
+
+data_path = Path(__file__).resolve().parents[4] / 'data/ingredients.csv'
+try:
+    with open(data_path, encoding='utf-8') as f:
+        data = [list(line) for line in csv.reader(f)]
+except Exception:
+    data = []
+
+
+def add_ingredients(apps, schema_editor):
+    MeasurementUnit = apps.get_model('recipes', 'MeasurementUnit')
+    Ingredient = apps.get_model('recipes', 'Ingredient')
+    for ingredient in data:
+        with transaction.atomic():
+            try:
+                measurement_unit = MeasurementUnit.objects.get(
+                    name=ingredient[1]
+                )
+                Ingredient.objects.create(
+                    name=ingredient[0],
+                    measurement_unit=measurement_unit
+                )
+            except:
+                continue
+
+
+def remove_ingredients(apps, schema_editor):
+    Ingredient = apps.get_model('recipes', 'Ingredient')
+    for ingredient in data:
+        try:
+            Ingredient.objects.get(
+                name=ingredient[0]
+            ).delete()
+        except Exception:
+            continue
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('recipes', '0004_add_measurement_unit'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            add_ingredients,
+            remove_ingredients
+        )
+    ]
